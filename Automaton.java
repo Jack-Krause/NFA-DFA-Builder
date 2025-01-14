03fcaeeefabe31225a0254c9c93e17e8ba60d263
Jack Krause <jmkrause@iastate.edu>
2025-01-14 14:01:16 -0600

    Automata definition added

--- Automaton.java
+++ Automaton.java
@@ -2,11 +2,18 @@ import java.util.ArrayList;
 
 public abstract class Automaton {
 
-    public Automaton(int[] input) { this.input = input; }
+    public Automaton(int[] Sigma) { this.Sigma = Sigma; }
 
-    int[] input;
-    ArrayList<State> states;
+    int[] Sigma;
+    ArrayList<State> Q; // All states
+    ArrayList<State> F; // Accepting states
 
     abstract boolean simulateInput();
+
+    /**
+     * delta: transition function
+     * @return State corresponding to the transition function
+     */
+    abstract State delta(State currentState, int bit);
     
 }
--- DFA.java
+++ DFA.java
@@ -1,12 +1,14 @@
 public class DFA extends Automaton {
 
+    private State s; // starting state
+
     public DFA(int[] input) {
         super(input);
     }
 
     @Override
     boolean simulateInput() {
-        for (int inp : this.input) {
+        for (int inp : this.Sigma) {
             for (int i = 0; i < 32; i++) {
                 int bit = (inp >> i) & 1;
 
@@ -20,6 +22,11 @@ public class DFA extends Automaton {
         }
 
         return false;
+    }
+
+    @Override
+    State delta(State currentState, int bit) {
+        return null;
     }
 
 }
